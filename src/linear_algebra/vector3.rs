c31511f737a614f8a6ebca710b2cd6a17781422b
Pietro Agnoli <87072344+TheCaptainCraken@users.noreply.github.com>
2023-11-14 13:13:17 +0100

    added tests

--- src/linear_algebra/vector3.rs
+++ src/linear_algebra/vector3.rs
@@ -3,7 +3,7 @@ use std::{
     ops::{Add, Div, Mul, Sub},
 };
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Vector3 {
     pub x: f64,
     pub y: f64,
@@ -47,5 +47,24 @@ impl Sub for Vector3 {
 impl Display for Vector3 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.write_fmt(format_args!("[{}, {}, {}]", self.x, self.y, self.z))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        let v1 = Vector3::new(1.0, 2.0, 3.0);
+        let v2 = Vector3::new(3.0, 2.0, 1.0);
+        let v3 = Vector3::new(f64::INFINITY, f64::NEG_INFINITY, 0.0);
+
+        let sum12 = v1 + v2;
+        let sum21 = v2 + v1;
+        let sum13 = v1 + v3;
+        assert_eq!(sum12, Vector3::new(4.0, 4.0, 4.0));
+        assert_eq!(sum12, sum21);
+        assert_eq!(sum13, Vector3::new(f64::INFINITY, f64::NEG_INFINITY, 3.0));
     }
 }
